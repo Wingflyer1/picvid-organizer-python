0bfe17040e6a8a3c4c7168318c9826cbe9fdf17d
Wingflyer1 <stentf@ntnu.no>
2017-02-06 22:47:13 +0100

    Added RegEx to skip allready renamed files

--- rename_file_by_month_year_v2.py
+++ rename_file_by_month_year_v2.py
@@ -3,6 +3,7 @@ from time import gmtime
 import time
 from PIL import Image
 import PIL.ExifTags
+import re
 
 top_folder = os.getcwd()
 dest_top = top_folder +'\\001_ordered_files\\'
@@ -34,11 +35,13 @@ def change_names():
         # iterate over each file in the directories and select video and picture files
         for file in filenames:
             count_files += 1
-            i = file.split('.')[-1]
-            if i.lower() in fil_ext:
-                # print(i.lower())
 
-
+            # check if file allready renamed, if so let it stay
+            pat_renamed_files = re.compile(r'^[01][0-9]{4}\s[0-9]+[-][12][0-9]{3}')
+            if pat_renamed_files.search(file) != None:
+                continue
+            i = file.split('.')[-1].lower()
+            if i in pic_ext:             
                 try:
                     count_vid_pic_files += 1
                     img = PIL.Image.open(str(path + '\\' + file))
@@ -67,19 +70,32 @@ def change_names():
                     # file_created_year = gmtime(file_stat.st_ctime).tm_year
                     # file_created_month = gmtime(file_stat.st_ctime).tm_mon
                 # src file and path name
-                src_file = "{}{}{}".format(path, '\\', file)
+#==============================================================================
+#                 src_file = "{}{}{}".format(path, '\\', file)
+# 
+#                 # dest file and path name
+# 
+#                 new_file_number = str(count_vid_pic_files).zfill(5)
+#                 new_file_name = "{} {}-{}.{}".format(new_file_number, file_created_month, file_created_year, file.split('.')[-1])
+#                 dest_file = "{}{}\{}\\{}".format(dest_top, file_created_year, file_created_month, new_file_name)
+# 
+#                 # src and dest into dictionary
+#                 pic_vid_files_to_move[src_file] = dest_file
+#==============================================================================
+            elif i in vid_ext:
+                coh = re.compile(r'[12][09][0-9]{2}[01][0-9][0-3][0-9]')
+                year = re.compile(r'[12][09][0-9]{2}')
+                print(file)
+                match_coh = coh.search(file)
+                match_year = year.search(file)
+                if match_coh != None:
+                    print(match_coh.group(), file)
+                elif match_year != None:
+                    print(match_year.group(), file)
 
-                # dest file and path name
-
-                new_file_number = str(count_vid_pic_files).zfill(5)
-                new_file_name = "{} {}-{}.{}".format(new_file_number, file_created_month, file_created_year, file.split('.')[-1])
-                dest_file = "{}{}\{}\\{}".format(dest_top, file_created_year, file_created_month, new_file_name)
-
-                # src and dest into dictionary
-                pic_vid_files_to_move[src_file] = dest_file
 
             else:
-                if file.split('.')[-1].lower() != "py" :
+                if i != "py" :
                     other_files_count += 1
                     src_other_file = "{}\\{}".format(path, file)
                     dst_other_file = "{}{}".format(dest_other, file)
@@ -116,15 +132,22 @@ def change_names():
     f.write(str(count_vid_pic_files))
     f.close()
 
-fil_ext = ['ani', 'bmp', 'cal', 'fax', 'gif', 'img', 'jbg', 'jpe',
-           'jpeg', 'jpg', 'mac', 'pbm', 'pcd', 'pcx', 'pct', 'pgm',
-           'png', 'ppm', 'psd', 'ras', 'tga', 'tiff', 'wmf', 'yuv',
-           'wmv', 'webm', 'vob', 'svi', 'roq', 'rmvb', 'rm', 'ogv',
-           'ogg', 'nsv', 'mxf', 'mpg', 'mpeg', 'm2v', 'mpg', 'mp2',
-           'mpeg', 'mpe', 'mpv', 'mp4', 'm4v', 'mov', 'qt', 'mng',
-           'mkv', 'm4v', 'gifv', 'gif', 'flv ', 'f4v ', 'f4p',
-           'f4a', 'f4b', 'flv', 'flv', 'drc', 'avi', 'asf', 'amv',
-           '3gp', '3g2']
+vid_ext =  ['ogg', 'avi', 'webm', 'f4p ', 'f4a', 'flv', '3gp', 'ppm',
+            'asf', 'f4b ', 'yuv', 'svi', 'm4v', 'ani', 'mac', 'f4a ',
+            'mpeg', 'drc', 'roq', 'mp2', 'ras', 'mpe', 'pcx', 'vob',
+            'mp4 ', 'pbm', 'mpg ', 'f4p', 'pcd', 'cal', 'jpe', 'wmf',
+            'flv ', 'mkv', 'amv', 'mpe ', 'ogv ', 'wmv', 'tga', 'jbg',
+            '3g2', 'mpeg ', 'mpv', 'mxf', 'qt', 'f4b', 'fax', 'gifv',
+            'm4p ', 'mpg', 'ogv', 'psd', 'pgm', 'm2v', 'nsv', 'mp4',
+            'rmvb', 'rm', 'mov, ', 'mov', 'f4v ']
+            
 
+
+pic_ext =  ['jpeg', 'jpg', 'webp', 'gif', 'png', 'napng', 'mng', 'tiff',
+            'svg', 'pdf', 'xbm', 'bmp', 'ico', 'pct']
+
+            
+            
+            
 if __name__ == "__main__":
     change_names()
